d63b357208277ba67a17154060c807e4fb6e517b
Christopher Chalmers <c.chalmers@me.com>
2014-06-20 01:09:31 +0100

    Added texinputs support.

--- System/TeXRunner.hs
+++ System/TeXRunner.hs
@@ -15,8 +15,10 @@ module System.TeXRunner
   ) where
 
 import Control.Applicative
-import Data.ByteString.Char8 as C8
+import Data.ByteString.Char8 as C8 hiding (intercalate, concatMap)
+-- import Data.List             (intercalate)
 import Data.Maybe
+import System.Environment
 import System.Exit
 import System.FilePath
 import System.IO
@@ -28,32 +30,36 @@ import System.TeXRunner.Parse
 -- | Run TeX program in a temporary system directory.
 runTex :: String     -- ^ TeX command
        -> [String]   -- ^ Additional arguments
+       -> [FilePath] -- ^ Additional TeX inputs
        -> ByteString -- ^ Source TeX file
        -> IO (ExitCode,
               Either String TeXLog,
               Maybe ByteString)
 
-runTex command args source =
+runTex command args extras source =
   withSystemTempDirectory "texrunner." $ \path ->
-    runTex' path command args source
+    runTex' path command args extras source
 
 runTex' :: FilePath   -- ^ Directory to run TeX in
         -> String     -- ^ TeX command
         -> [String]   -- ^ Additional arguments
+        -> [FilePath] -- ^ Additional TeX inputs
         -> ByteString -- ^ Source TeX file
         -> IO (ExitCode,
                Either String TeXLog,
                Maybe ByteString)
 
-runTex' path command args source = do
+runTex' path command args extras source = do
 
   C8.writeFile (path </> "texrunner.tex") source
+
+  environment <- extraTeXInputs (path:extras) <$> getEnvironment
 
   let p = (proc command ("texrunner.tex" : args))
             { cwd     = Just path
             , std_in  = CreatePipe
             , std_out = CreatePipe
-            , env     = Nothing -- Add dirs to "TEXINPUTS"
+            , env     = Just environment
             }
 
   (Just inH, Just outH, _, pHandle) <- createProcess p
@@ -68,4 +74,27 @@ runTex' path command args source = do
   logFile <- optional $ C8.readFile (path </> "texrunner.log")
 
   return (exitC, parseLog $ fromMaybe a logFile, pdfFile)
+
+-- | Add a list of paths to the tex 
+extraTeXInputs :: [FilePath] -> [(String,String)] -> [(String,String)]
+extraTeXInputs []      = id
+extraTeXInputs inputss = alter f "TEXINPUTS"
+  where
+    f Nothing  = Just inputs
+    f (Just x) = Just (inputs ++ [searchPathSeparator] ++ x)
+    --
+    inputs = concatMap (++ [searchPathSeparator]) inputss
+    -- inputs = intercalate [searchPathSeparator] inputss
+
+-- Alter can be used to insert, delete or update an element. Similar to alter
+-- in Data.Map.
+alter :: Eq k => (Maybe a -> Maybe a) -> k -> [(k,a)] -> [(k,a)]
+alter f k = go
+  where
+    -- go :: [(k,a)] -> [(k,a)]
+    go [] = maybeToList ((,) k <$> f Nothing)
+    go ((k',x):xs) | k' == k   = case f (Just x) of
+                                   Just x' -> (k',x') : xs
+                                   Nothing -> xs
+                   | otherwise = (k',x) : go xs
 
